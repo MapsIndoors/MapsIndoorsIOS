7e0724a1a380450f0c9438d5ba3c2236b5bccd4a
Daniel-Nielsen <dn@mapspeople.com>
2016-09-07 10:12:09 +0200

    Forced error in credentials file

--- Example/MapsIndoors/AppDelegate.h
+++ Example/MapsIndoors/AppDelegate.h
@@ -9,7 +9,7 @@
 #define kMapsIndoorsSolutionId @"550c26a864617400a40f0000"                  //ADD YOUR OWN MAPSINDOORS SOLUTION ID
 #define kVenue @"rtx"                                                       //OPTIONALLY ADD A VENUE NAME
 #define kGoogleMapsApiKey @"AIzaSyDwkz2YVlxoJnjhSMRp_qsLlBXLgQEArAc"        //ADD YOUR OWN GOOGLE MAPS IOS API KEY
-//ADD_YOUR_OWN_GOOGLE_DIRECTIONS_API_KEY
+ADD_YOUR_OWN_GOOGLE_DIRECTIONS_API_KEY
 #define kGoogleDirectionsApiKey @"ADD_YOUR_OWN_GOOGLE_DIRECTIONS_API_KEY"   //ADD YOUR OWN GOOGLE MAPS DIRECTIONS API KEY
 
 #import <UIKit/UIKit.h>
