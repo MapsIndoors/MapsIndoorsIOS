8b362ae033e79b90050299e16d7d422c33219a5a
Daniel Nielsen <dn@mapspeople.com>
2017-02-15 20:25:25 +0100

    Updated build phase scripts

--- Scripts/derive_ressources.sh
+++ Scripts/derive_ressources.sh
@@ -16,8 +16,7 @@ for relUrl in "/sync/venues" "/sync/buildings" "/sync/appconfig" "/sync/solution
 done
 
 
-#for fileName in "res/mi_sync_solutions.json" "res/mi_sync_tiles.json" "res/mi_sync_locations.json" "res/mi_sync_appconfig.json"; do
-for fileName in "${SRCROOT}/res/mi_sync_solutions.json" "${SRCROOT}/res/mi_sync_tiles.json" "${SRCROOT}/res/mi_sync_appconfig.json"; do
+for fileName in "${SRCROOT}/res/mi_sync_solutions.json" "${SRCROOT}/res/mi_sync_tiles.json" "${SRCROOT}/res/mi_sync_locations.json" "${SRCROOT}/res/mi_sync_appconfig.json"; do
     grep -oE '\b(https?)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]' $fileName | while read url
     do
         path="$(echo $url | cut -d '/' -f4-)"
--- Scripts/strip-frameworks.sh
+++ Scripts/strip-frameworks.sh
@@ -1,28 +1,29 @@
-APP_PATH="${TARGET_BUILD_DIR}/${WRAPPER_NAME}"
+FRMW_PATH="${TARGET_BUILD_DIR}/${WRAPPER_NAME}/Frameworks/"
 
-# This script loops through the frameworks embedded in the application and
-# removes unused architectures.
-find "$APP_PATH/." -name '[MapsIndoorsSwift|Gloss|Zip].framework' -type d | while read -r FRAMEWORK
-do
-FRAMEWORK_EXECUTABLE_NAME=$(defaults read "$FRAMEWORK/Info.plist" CFBundleExecutable)
-FRAMEWORK_EXECUTABLE_PATH="$FRAMEWORK/$FRAMEWORK_EXECUTABLE_NAME"
-echo "Executable is $FRAMEWORK_EXECUTABLE_PATH"
+echo "Looping through the frameworks embedded in the application and removes unused architectures."
+echo "Looking in $FRMW_PATH"
 
-EXTRACTED_ARCHS=()
+for FRAMEWORK in "$FRMW_PATH/MapsIndoorsSwift.framework" "$FRMW_PATH/Gloss.framework" "$FRMW_PATH/Zip.framework"
+    do
+    FRAMEWORK_EXECUTABLE_NAME=$(defaults read "$FRAMEWORK/Info.plist" CFBundleExecutable)
+    FRAMEWORK_EXECUTABLE_PATH="$FRAMEWORK/$FRAMEWORK_EXECUTABLE_NAME"
+    echo "Executable is $FRAMEWORK_EXECUTABLE_PATH"
 
-for ARCH in $ARCHS
-do
-echo "Extracting $ARCH from $FRAMEWORK_EXECUTABLE_NAME"
-lipo -extract "$ARCH" "$FRAMEWORK_EXECUTABLE_PATH" -o "$FRAMEWORK_EXECUTABLE_PATH-$ARCH"
-EXTRACTED_ARCHS+=("$FRAMEWORK_EXECUTABLE_PATH-$ARCH")
-done
+    EXTRACTED_ARCHS=()
 
-echo "Merging extracted architectures: ${ARCHS}"
-lipo -o "$FRAMEWORK_EXECUTABLE_PATH-merged" -create "${EXTRACTED_ARCHS[@]}"
-rm "${EXTRACTED_ARCHS[@]}"
+    for ARCH in $ARCHS
+        do
+        echo "Extracting $ARCH from $FRAMEWORK_EXECUTABLE_NAME"
+        lipo -extract "$ARCH" "$FRAMEWORK_EXECUTABLE_PATH" -o "$FRAMEWORK_EXECUTABLE_PATH-$ARCH"
+        EXTRACTED_ARCHS+=("$FRAMEWORK_EXECUTABLE_PATH-$ARCH")
+    done
 
-echo "Replacing original executable with thinned version"
-rm "$FRAMEWORK_EXECUTABLE_PATH"
-mv "$FRAMEWORK_EXECUTABLE_PATH-merged" "$FRAMEWORK_EXECUTABLE_PATH"
+    echo "Merging extracted architectures: ${ARCHS}"
+    lipo -o "$FRAMEWORK_EXECUTABLE_PATH-merged" -create "${EXTRACTED_ARCHS[@]}"
+    rm "${EXTRACTED_ARCHS[@]}"
+
+    echo "Replacing original executable with thinned version"
+    rm "$FRAMEWORK_EXECUTABLE_PATH"
+    mv "$FRAMEWORK_EXECUTABLE_PATH-merged" "$FRAMEWORK_EXECUTABLE_PATH"
 
 done
