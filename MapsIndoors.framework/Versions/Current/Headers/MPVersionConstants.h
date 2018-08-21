a73d189a9ec0621e445485b7375fe1a44f408c96
Michael Bech Hansen <mbh@mapspeople.com>
2018-08-21 09:03:20 +0200

    Release commit, version 2.1.0, see CHANGELOG.md

--- MapsIndoors.framework/Versions/Current/Headers/MPVersionConstants.h
+++ MapsIndoors.framework/Versions/Current/Headers/MPVersionConstants.h
@@ -1,6 +1,6 @@
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"2.1.0-beta13"
+#define MI_SDK_VERSION        @"2.1.0"
 #define MI_SDK_VERSION_MAJOR  2
 #define MI_SDK_VERSION_MINOR  1
 #define MI_SDK_VERSION_PATCH  0
-#define MI_SDK_VERSION_LABEL  @"beta13"
+#define MI_SDK_VERSION_LABEL  @""
