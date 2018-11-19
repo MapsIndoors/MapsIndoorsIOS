9d71bbde9caf118ca96d03fed741a4c6ff7199d4
Daniel Nielsen <dn@mapspeople.com>
2018-11-19 11:50:17 +0100

    Added bac Pods dir

---
+++ Example/Pods/Target Support Files/GoogleFontsiOS/GoogleFontsiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GoogleFontsiOS : NSObject
+@end
+@implementation PodsDummy_GoogleFontsiOS
+@end
---
+++ Example/Pods/Target Support Files/VCMaterialDesignIcons/VCMaterialDesignIcons-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VCMaterialDesignIcons : NSObject
+@end
+@implementation PodsDummy_VCMaterialDesignIcons
+@end
