f4eed84e0577f736b3cfeae28d8be85191330097
Daniel Nielsen <dn@mapspeople.com>
2018-11-28 16:13:48 +0100

    Release commit, version 2.1.6, see CHANGELOG.md

--- MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
+++ MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
@@ -1,6 +1,6 @@
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"2.1.6-rc11"
+#define MI_SDK_VERSION        @"2.1.6"
 #define MI_SDK_VERSION_MAJOR  2
 #define MI_SDK_VERSION_MINOR  1
 #define MI_SDK_VERSION_PATCH  6
-#define MI_SDK_VERSION_LABEL  @"rc11"
+#define MI_SDK_VERSION_LABEL  @""
