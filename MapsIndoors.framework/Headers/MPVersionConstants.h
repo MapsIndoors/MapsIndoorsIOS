797aff45db06c55eb581c25421d4d60e03014461
Michael Bech Hansen <mbh@mapspeople.com>
2019-01-11 15:35:00 +0100

    Release commit, version 3.0.0-alpha40, see CHANGELOG.md

--- MapsIndoors.framework/Headers/MPVersionConstants.h
+++ MapsIndoors.framework/Headers/MPVersionConstants.h
@@ -1,6 +1,6 @@
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"3.0.0-alpha39"
+#define MI_SDK_VERSION        @"3.0.0-alpha40"
 #define MI_SDK_VERSION_MAJOR  3
 #define MI_SDK_VERSION_MINOR  0
 #define MI_SDK_VERSION_PATCH  0
-#define MI_SDK_VERSION_LABEL  @"alpha39"
+#define MI_SDK_VERSION_LABEL  @"alpha40"
