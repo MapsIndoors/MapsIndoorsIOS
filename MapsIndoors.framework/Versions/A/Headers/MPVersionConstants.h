b7b662728fc76356fc95564f49d3bd410757d44d
CI User <dn@mapspeople.com>
2019-07-01 08:59:33 +0200

    Release commit, version 3.1.2-rc9, see CHANGELOG.md

--- MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
+++ MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
@@ -1,6 +1,6 @@
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"3.1.2-rc4"
+#define MI_SDK_VERSION        @"3.1.2-rc9"
 #define MI_SDK_VERSION_MAJOR  3
 #define MI_SDK_VERSION_MINOR  1
 #define MI_SDK_VERSION_PATCH  2
-#define MI_SDK_VERSION_LABEL  @"rc4"
+#define MI_SDK_VERSION_LABEL  @"rc9"
