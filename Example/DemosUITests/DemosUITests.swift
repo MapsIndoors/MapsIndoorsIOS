b4d543e4eb790600f092c5d4649f492cd6fe6398
Daniel Nielsen <dn@mapspeople.com>
2019-08-21 13:59:26 +0200

    Updated UI Test

--- Example/DemosUITests/DemosUITests.swift
+++ Example/DemosUITests/DemosUITests.swift
@@ -9,7 +9,13 @@
 import XCTest
 
 class DemosUITests: XCTestCase {
-        
+    
+    var peakMemUsage = 0.0
+    var accumulatedMem = 0.0
+    let avgMemLimit = 180.0
+    let memLimit = 250.0
+    var numberOfMemSamples = 0.0
+    
     override func setUp() {
         super.setUp()
         
@@ -17,81 +23,106 @@ class DemosUITests: XCTestCase {
         XCUIApplication().launch()
     }
     
-    fileprivate func wait(_ app: XCUIApplication) {
-        _ = app.wait(for: .notRunning, timeout: 1)
+    fileprivate func assertMemoryUsageTimespan(_ app: XCUIApplication) {
+        assertMemoryUsage(app)
+        _ = app.wait(for: .notRunning, timeout: 0.5)
+        assertMemoryUsage(app)
+        _ = app.wait(for: .notRunning, timeout: 0.5)
     }
     
-    func testSet1() {
+    fileprivate func assertMemoryUsage(_ app: XCUIApplication) {
+        numberOfMemSamples += 1.0
+        let memoryLabel = app.staticTexts["MemoryFootprint"]
+        XCTAssert(memoryLabel.exists)
+        let memoryAsDouble = (memoryLabel.label as NSString).doubleValue
+        accumulatedMem += memoryAsDouble
+        peakMemUsage = max(peakMemUsage, memoryAsDouble)
+        
+        XCTAssert(accumulatedMem / numberOfMemSamples < avgMemLimit, "Average memory consumption exceeded limit of \(avgMemLimit)mb")
+        XCTAssert(peakMemUsage < memLimit, "Peak memory consumption exceeded limit of \(memLimit)mb")
+    }
+    
+    func testDemoSamples() {
         
         let app = XCUIApplication()
         let tablesQuery = app.tables
         
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Show Location Demo"]/*[[".cells.staticTexts[\" Show Location Demo\"]",".staticTexts[\" Show Location Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        tablesQuery.staticTexts["Show Location Demo"].tap()
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Location Details Demo"]/*[[".cells.staticTexts[\" Location Details Demo\"]",".staticTexts[\" Location Details Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Show Multiple Locations Demo"]/*[[".cells.staticTexts[\" Show Multiple Locations Demo\"]",".staticTexts[\" Show Multiple Locations Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Show Route Demo"]/*[[".cells.staticTexts[\" Show Route Demo\"]",".staticTexts[\" Show Route Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         tablesQuery.staticTexts["Show leg 0, all steps"].tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Show Venue Demo"]/*[[".cells.staticTexts[\" Show Venue Demo\"]",".staticTexts[\" Show Venue Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Show Building Demo"]/*[[".cells.staticTexts[\" Show Building Demo\"]",".staticTexts[\" Show Building Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Show Floor Demo"]/*[[".cells.staticTexts[\" Show Floor Demo\"]",".staticTexts[\" Show Floor Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Change Display Setting Demo"]/*[[".cells.staticTexts[\" Change Display Setting Demo\"]",".staticTexts[\" Change Display Setting Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Custom Floor Selector Demo"]/*[[".cells.staticTexts[\" Custom Floor Selector Demo\"]",".staticTexts[\" Custom Floor Selector Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        tablesQuery.staticTexts["Custom Floor Selector Demo"].tap()
+        assertMemoryUsageTimespan(app)
+        app.pinch(withScale: 1.2, velocity: 0.5)
+        assertMemoryUsageTimespan(app)
         app.navigationBars["Stigsborgvej - Floor 0"].buttons["Floor"].tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.alerts["Select Floor"].buttons["Floor 1 "].tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
-
+        
         app.tables/*@START_MENU_TOKEN@*/.staticTexts["Show My Location Demo"]/*[[".cells.staticTexts[\" Show My Location Demo\"]",".staticTexts[\" Show My Location Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
         
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Search Map Demo"]/*[[".cells.staticTexts[\" Search Map Demo\"]",".staticTexts[\" Search Map Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.buttons["Search"].tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.searchFields.firstMatch.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.searchFields.firstMatch.typeText("park")
-        tablesQuery.staticTexts["Parking 1, , , Stigsborgvej"].tap()
-        wait(app)
+        tablesQuery.staticTexts["Guest Parking, , , Stigsborgvej"].tap()
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
-
+        
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Multiple Datasets Demo"]/*[[".cells.staticTexts[\" Multiple Datasets Demo\"]",".staticTexts[\" Multiple Datasets Demo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Stigsborgvej"]/*[[".cells.staticTexts[\"Stigsborgvej\"]",".staticTexts[\"Stigsborgvej\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
         let backButton = app.navigationBars["Demos.DatasetView"].buttons["Back"]
         backButton.tap()
+        assertMemoryUsageTimespan(app)
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Aalborg City"]/*[[".cells.staticTexts[\"Aalborg City\"]",".staticTexts[\"Aalborg City\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        assertMemoryUsageTimespan(app)
         backButton.tap()
-        wait(app)
+        assertMemoryUsageTimespan(app)
         app.navigationBars.buttons.firstMatch.tap()
+        
+        assertMemoryUsageTimespan(app)
+        
+        print("\(#function): Average memory usage:  \(accumulatedMem/numberOfMemSamples)")
+        print("\(#function): Peak memory usage:     \(peakMemUsage)")
+        
     }
 }
