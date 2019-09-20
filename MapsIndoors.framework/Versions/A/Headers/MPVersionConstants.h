eb9bd3f8d1351b619877e63156f1f8689b2584dc
CI User <dn@mapspeople.com>
2019-09-20 12:04:18 +0200

    Framework commit, version 3.4.0-beta3, see CHANGELOG.md

--- MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
+++ MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
@@ -1,6 +1,6 @@
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"3.4.0-beta2"
+#define MI_SDK_VERSION        @"3.4.0-beta3"
 #define MI_SDK_VERSION_MAJOR  3
 #define MI_SDK_VERSION_MINOR  4
 #define MI_SDK_VERSION_PATCH  0
-#define MI_SDK_VERSION_LABEL  @"beta2"
+#define MI_SDK_VERSION_LABEL  @"beta3"
