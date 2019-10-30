ca8ba65ea9be398382296c0ab03daab93063113d
Daniel Nielsen <dn@mapspeople.com>
2019-10-30 13:57:31 +0100

    Framework commit, version 2.2.3

--- MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
+++ MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
@@ -1,6 +1,6 @@
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"2.2.2"
+#define MI_SDK_VERSION        @"2.2.3"
 #define MI_SDK_VERSION_MAJOR  2
 #define MI_SDK_VERSION_MINOR  2
-#define MI_SDK_VERSION_PATCH  2
+#define MI_SDK_VERSION_PATCH  3
 #define MI_SDK_VERSION_LABEL  @""
