5e8605286a47482f81784797bdb9617d9f773e05
Daniel Nielsen <dn@mapspeople.com>
2020-10-08 15:37:29 +0200

    Framework commit, version 3.12.1, see CHANGELOG.md

--- MapsIndoors.framework/Headers/MPVersionConstants.h
+++
@@ -1,6 +0,0 @@
-// Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"3.12.0"
-#define MI_SDK_VERSION_MAJOR  3
-#define MI_SDK_VERSION_MINOR  12
-#define MI_SDK_VERSION_PATCH  0
-#define MI_SDK_VERSION_LABEL  @""
