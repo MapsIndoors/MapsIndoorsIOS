0c7a664c43af9deb5c60248000edb06c052343dd
Daniel Nielsen <dn@mapspeople.com>
2021-06-01 14:07:16 +0200

    Remove fat framework

--- MapsIndoors.framework/Versions/A/Headers/MPVersionConstants.h
+++
@@ -1,6 +0,0 @@
-// Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"3.28.0"
-#define MI_SDK_VERSION_MAJOR  3
-#define MI_SDK_VERSION_MINOR  28
-#define MI_SDK_VERSION_PATCH  0
-#define MI_SDK_VERSION_LABEL  @""
--- MapsIndoors.framework/Versions/A/Headers/NSURL+QueryParser.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSURL (QueryParser)
-
-- (nonnull NSDictionary*)queryDictionary;
-
-@end
-
