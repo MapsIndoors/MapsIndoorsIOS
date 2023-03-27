b561acbc67d0df5ce468d2ae66c30a6fa9293ef5
Christian Wolf Johannsen <chwo@mapspeople.com>
2023-03-27 14:26:56 +0200

    Framework commit, version 3.43.3-sc, see CHANGELOG.md

--- MapsIndoors.xcframework/ios-arm64/MapsIndoors.framework/Headers/MPVersionConstants.h
+++ MapsIndoors.xcframework/ios-arm64/MapsIndoors.framework/Headers/MPVersionConstants.h
@@ -3,8 +3,8 @@
 
 
 // Auto-generated file - Do not modify
-#define MI_SDK_VERSION        @"3.43.3-sc2"
+#define MI_SDK_VERSION        @"3.43.3-sc"
 #define MI_SDK_VERSION_MAJOR  3
 #define MI_SDK_VERSION_MINOR  43
 #define MI_SDK_VERSION_PATCH  3
-#define MI_SDK_VERSION_LABEL  @"sc2"
+#define MI_SDK_VERSION_LABEL  @"sc"
